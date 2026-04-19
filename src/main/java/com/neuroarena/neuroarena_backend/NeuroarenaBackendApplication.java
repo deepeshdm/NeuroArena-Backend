cb51d429f9b23359c415cfb874974f24e2a699fd
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2026-04-19 12:16:36 +0530

    Added h2 database setup and configire env

--- src/main/java/com/neuroarena/neuroarena_backend/NeuroarenaBackendApplication.java
+++ src/main/java/com/neuroarena/neuroarena_backend/NeuroarenaBackendApplication.java
@@ -1,5 +1,5 @@
 package com.neuroarena.neuroarena_backend;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NeuroarenaBackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(NeuroarenaBackendApplication.class, args);
 	}
 
