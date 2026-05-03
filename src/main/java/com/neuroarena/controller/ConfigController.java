fc46e602f210c75d5cb9b14324d0e25c034cee8e
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2026-05-03 18:43:39 +0530

    added config

---
+++ src/main/java/com/neuroarena/controller/ConfigController.java
@@ -0,0 +1,35 @@
+// ConfigController.java
+package com.neuroarena.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/config")
+public class ConfigController {
+
+    @Value("${game.max-players}")
+    private int maxPlayers;
+
+    @Value("${game.questions-per-battle}")
+    private int questionsPerBattle;
+
+    @Value("${game.question-time-limit}")
+    private int questionTimeLimit;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put("maxPlayers", maxPlayers);
+        config.put("questionsPerBattle", questionsPerBattle);
+        config.put("questionTimeLimit", questionTimeLimit);
+
+        return ResponseEntity.ok(config);
+    }
+}
